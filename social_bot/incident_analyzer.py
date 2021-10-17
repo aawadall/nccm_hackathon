e41174d361f049b539c6f98343ec6b53427d70fb
Ahmed Awadallah <aawadall@ualberta.ca>
2021-10-16 20:08:04 -0400

    more stable

--- social_bot/incident_analyzer.py
+++ social_bot/incident_analyzer.py
@@ -2,8 +2,9 @@
 
 
 import datetime
-from os import urandom
+import random
 from operator import itemgetter
+from operator import add
 
 
 class HateIncidentAnalyzer:
@@ -21,26 +22,35 @@ class HateIncidentAnalyzer:
         # build some random vector of violations
         violations = [0] * len(self.topics)
         for i in range(len(self.topics)):
-            if self.topics[i] in tweet:
-                violations[i] = urandom.randint(0, 1)
+            violations[i] = random.uniform(0, 1)
+            # if self.topics[i] in tweet:
+            #     # random number between 0 and 1
+            #     violations[i] = 0.5 + random.uniform(0, 1)
         return violations
 
     def time_weighted_analysis(self, violations):
         """Given a list of violations for a particular user:
                 returns time weighted analysis of hate incidents"""
         print('Time Weighted Analysis')
+
         # violations made of a list of user, timestamp, and violations
         # use most recent violation as baseline (maximum timestamp)
         baseline = get_time_from_string(max(violations, key=itemgetter(1))[1])
         print('Baseline: {}'.format(baseline))
         # for each violation, calculate weight and multiply by violation
         weighted_violations = [0] * len(self.topics)
+
         for violation in violations:
             print('Calculating weight for violation: {}'.format(violation))
+
             delta_t = baseline - get_time_from_string(violation[1])
             discount = self.discount_factor ** (float(delta_t.seconds)/60)
-            el = [v * discount for v in violation]
-            weighted_violations = weighted_violations + el
+            el = [v * discount for v in violation[2]]
+
+            weighted_violations = [sum(x)
+                                   for x in zip(weighted_violations, el)]
+            print('Weighted Violations: ')
+            print(weighted_violations)
         # return sum of weights
         return weighted_violations
 
--- social_bot/thresholds.py
+++ social_bot/thresholds.py
@@ -17,15 +17,23 @@ def get_escalations(violations):
     """from violations vector, find escalation level for each categoty"""
     result = {}
     category = 0
+
+    print("violations")
+    print(violations)
     for violation in violations:
         # take a slice of the escalation matrix
+        print(violation)
         escalation_slice = escalation_matrix[category]
         # find the escalation level where the violation is greater than the threshold and and the next level
-        escalation_level = escalation_slice.index(
-            max(escalation_slice[:violation]))
-        result[hate_categories[category]] = {
-            "level": escalation_level,
-            "response": media_library[category][escalation_level]}
+        # escalation_level = escalation_slice.index(
+        #     max(escalation_slice[:violation]))
+        escalation_level = [idx for idx in range(
+            len(escalation_slice)) if escalation_slice[idx] < violation]
+        print(escalation_level)
+        if escalation_level:
+            result[hate_categories[category]] = {
+                "level": escalation_level,
+                "response": media_library[category][escalation_level]}
 
         # increment the category
         category += 1
